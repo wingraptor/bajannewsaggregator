e3c1318a5c5897230a0c3e37861196e67a303496
Akono <akonobrathwaite@gmail.com>
2021-04-25 06:04:51 -0400

    Add nav bar and placeholder logo

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,16 +1,22 @@
 <template>
- <h1>Hello</h1>
+	<div>
+		<!-- As a link -->
+		<b-navbar variant="faded" type="dark" class="border-bottom">
+			<b-navbar-brand href="/">
+				<img src="@/assets/logo.svg" alt="" height="30" />
+				News Aggregator
+			</b-navbar-brand>
+		</b-navbar>
+	</div>
 </template>
 
 
 <script>
 export default {
-  name: 'NavBar',
-
-}
+	name: "NavBar",
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
 </style>
